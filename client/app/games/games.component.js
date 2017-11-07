c565ffec5bd48e3c2f465b9b14c45a491fa23357
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2017-11-07 17:27:44 +0530

    table structure and READ from CRUD is done.

--- client/app/games/games.component.js
+++ client/app/games/games.component.js
@@ -1,22 +1,38 @@
 'use strict';
 const angular = require('angular');
 const ngRoute = require('angular-route');
-
-
 import routes from './games.routes';
 
 export class GamesComponent {
+  games = [];
   /*@ngInject*/
-  constructor() {
-    this.message = 'Hello';
+  constructor($http, $scope, socket) {
+    this.$http = $http;
+    this.socket = socket;
+
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('games');
+    });
   }
+
+  $onInit() {
+    this.$http.get('/api/games')
+      .then(response => {
+        this.games = response.data;        
+        this.socket.syncUpdates('games', this.games);
+      })
+      .error(function(err) {
+        alert('Error! Something went wrong');
+      });;
+  }
+
 }
 
 export default angular.module('emsApp.games', [ngRoute])
   .config(routes)
   .component('games', {
     template: require('./games.html'),
-    controller: GamesComponent,
-    controllerAs: 'gamesCtrl'
+    controller: GamesComponent
+    // controllerAs: 'gamesCtrl'
   })
   .name;
